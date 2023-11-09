79b93bcdd350c07fa864d29f6ceb2f71182b1087
YoussraDhi <youssra@groover.co>
2023-11-09 14:15:37 +0100

    chore: init VButtonWrapper

---
+++ components/Buttons/VButtonWrapper.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineEmits, defineProps } from 'vue'
+
+type Props = {
+  isDisabled?: boolean
+}
+
+defineEmits(['click'])
+
+defineProps<Props>()
+</script>
+
+<template>
+  <button class="tw" @click="$emit('click', $event)">
+    <slot></slot>
+  </button>
+</template>
