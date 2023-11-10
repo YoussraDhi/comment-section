dfa2c9f0561636b6806b032319123b94e54ce217
YoussraDhi <youssra@groover.co>
2023-11-10 16:35:05 +0100

    Chore: build VAvatar and VInputText

---
+++ components/Avatar/VAvatar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineEmits, defineProps } from 'vue'
+
+type Props = {
+  pictureUrl: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div
+    class="tw-inline-flex tw-items-center tw-justify-center tw-h-10 tw-w-10 tw-rounded-full tw-border tw-border-indigo-700"
+  >
+    <img :src="props.pictureUrl" :class="'tw-rounded-full'" />
+  </div>
+  <slot></slot>
+</template>
---
+++ components/Inputs/VInputText.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue'
+
+type Props = {
+  label: string
+  value: string
+}
+
+const props = defineProps<Props>()
+
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div>
+    <label
+      for="input-text"
+      class="tw-block tw-text-gray-700 tw-font-bold tw-mb-2"
+      >{{ props.label }}</label
+    >
+    <input
+      id="input-text"
+      name="input-text"
+      class="tw-shadow tw-appearance-none tw-border tw-rounded tw-w-full tw-py-2 tw-px-3 tw-text-gray-700 tw-leading-tight tw-focus:tw-outline-none tw-focus:tw-shadow-outline tw-h-10"
+      modelValue="props.value"
+      @input="
+        $emit(
+          'update:modelValue',
+          ($event.target as HTMLTextAreaElement)?.value || ''
+        )
+      "
+    />
+  </div>
+</template>
